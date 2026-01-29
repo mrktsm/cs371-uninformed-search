482d69d5faca81de843c55f90472042f2e462094
Marko Tsymbaliuk <markotsymbaluk@gmail.com>
2026-01-29 02:02:48 -0500

    fix: current limit initiation

--- IterativeDeepeningDepthFirstSearcher.java
+++ IterativeDeepeningDepthFirstSearcher.java
@@ -2,7 +2,7 @@ public class IterativeDeepeningDepthFirstSearcher extends Searcher {
 
     public boolean search(SearchNode rootNode) {
         nodeCount = 0;
-        int currentLimit = 0;
+        int currentLimit = 1;
 
         while (true) {
             DepthLimitedSearcher dls = new DepthLimitedSearcher(currentLimit);
